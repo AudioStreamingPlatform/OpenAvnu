9b54c83fc73bc3d6298993a03140bf45e9fd1aa9
manszigher <extzig@bang-olufsen.dk>
2018-03-09 15:38:20 +0100

    Fixing build error when bumping up the poky version to rocko (#2)
    We shouldn't include the linux/timex.h since that will cause
    redefentions of structs. Instead we should use sys/timex.h.

--- daemons/gptp/linux/src/linux_hal_generic_adj.cpp
+++ daemons/gptp/linux/src/linux_hal_generic_adj.cpp
@@ -31,7 +31,7 @@
 
 ******************************************************************************/
 
-#include <linux/timex.h>
+#include <sys/timex.h>
  // avoid indirect inclusion of time.h since this will clash with linux/timex.h
 #define _TIME_H  1
 #define _STRUCT_TIMEVAL 1
